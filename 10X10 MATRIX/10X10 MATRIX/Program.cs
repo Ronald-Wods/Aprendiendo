b490232c5d1582cb63ab1dbbfce37d192629aedb
Ronald-Wods <Rynoged@gmail.com>
2025-07-17 20:08:49 +0100

    dolor

---
+++ 10X10 MATRIX/10X10 MATRIX/Program.cs
@@ -0,0 +1,22 @@
+﻿bool ya = false;
+
+int numero = 0;
+
+while (!ya)
+{
+
+
+    Console.Write($" {numero, 10} ");
+
+    numero++;
+
+    if (numero == 100)
+    {
+        ya = true;
+       
+
+    }
+}
+
+
+
