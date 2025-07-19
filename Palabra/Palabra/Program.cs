9c27b98d6c8c11bae35d8e54eaa91ce26f393463
Ronald-Wods <Rynoged@gmail.com>
2025-07-19 14:20:35 +0100

    dolor

---
+++ Palabra/Palabra/Program.cs
@@ -0,0 +1,24 @@
+﻿bool correcto = false;
+
+Console.WriteLine("Adivina la palabra, te doy una pista: Empieza por F");
+
+do
+{
+    switch (Console.ReadLine())
+    {
+        case "flor":
+            Console.WriteLine("Muy bien");
+            correcto = true;
+            break;
+        case "Flor":
+            Console.WriteLine("Muy bien");
+            correcto = true;
+            break;
+
+        default:
+            Console.WriteLine("NUH UH, inténtalo otra vez");
+            break;
+    }
+
+
+} while (!correcto);
