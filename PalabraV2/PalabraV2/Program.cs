a104abf05e9a8a971ce6d1cecfe88141af84f96e
Ronald-Wods <Rynoged@gmail.com>
2025-07-19 14:38:21 +0100

    dolor

---
+++ PalabraV2/PalabraV2/Program.cs
@@ -0,0 +1,22 @@
+﻿bool correcto = false;
+Console.WriteLine("Adivina la palabra, aquí una pista: empieza por L");
+
+do
+{
+    string palabra = Console.ReadLine().ToUpper();
+
+    if(palabra == "LAGO")
+    {
+        Console.WriteLine("Muy bien");
+        correcto = true;
+
+    }
+
+    else
+    {
+        Console.WriteLine("NUH UH, inténtalo otra vez");
+
+    }
+
+} while (!correcto);
+
