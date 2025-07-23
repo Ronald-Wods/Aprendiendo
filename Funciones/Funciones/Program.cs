8d3a57ea69897b91e17832b38019a15287281af1
Ronald-Wods <Rynoged@gmail.com>
2025-07-23 16:50:32 +0100

    dolor

---
+++ Funciones/Funciones/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Buenas Tardes\n");
+        Console.WriteLine("¿Qué tal estás?\n");
+        string respuesta = Console.ReadLine();
+
+        Pruebas(respuesta);
+    }
+    private static void Pruebas(string respuesta)
+    {
+        Console.WriteLine($"\n¿{respuesta}?");
+    }
+}
