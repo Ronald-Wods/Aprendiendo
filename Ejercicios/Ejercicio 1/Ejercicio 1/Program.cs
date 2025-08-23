9d7ce43cdf1173d805687b0fed3f94d0d5d5d2dd
Ronald-Wods <Rynoged@gmail.com>
2025-08-23 17:17:33 +0100

    Ejercicios

---
+++ Ejercicios/Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Método 1: Variables fijas");
+
+string Nombre1 = "Mike";
+int Edad1 = 24;
+string Address1 = "Whitechapel, London";
+
+Console.WriteLine($"\nUsuario: \"{Nombre1}\"\nEdad: \"{Edad1}\"\nDirección: \"{Address1}\"");
+
+Console.WriteLine($"__________________________________");
+
+Console.WriteLine("\nMétodo 2: Consola"); 
+
+Console.WriteLine("\nPara registrarse siga las siguientes indicaciones:");
+Console.WriteLine("\nProporcione su nombre");
+string nombre2 = Console.ReadLine();
+
+Console.WriteLine("\nProporcione su edad");
+int edad2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nProporcione su dirección");
+string address2 = Console.ReadLine();
+
+Console.WriteLine("\nBienvenido");
+Console.WriteLine($"\nUsuario: \"{nombre2}\"\nEdad: \"{edad2}\"\nDirección: \"{address2}\"");
+
+
+
+
+
+
---
+++ Ejercicios/Ejercicio 32/Ejercicio 32/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+class Program
+{ 
+    static void Main()
+    {
+        Menu();
+    }
+
+    static void Menu()
+    { 
+        bool exit = false;
+
+        do
+        {
+            Console.WriteLine("\nEscribe \'1\' para proporcionar una oración o escribe \'2\' para acabar la sesión");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    Mayus();
+                    break;
+
+                case 2:
+                    Console.WriteLine("\nFin de la sesión");
+                    exit = true;
+                    break;
+
+                default:
+                    Console.WriteLine("\nERROR: Opción desconocida, íntentalo de nuevo");
+                    break;
+            }
+
+        } while (!exit);
+
+    }
+
+    private static void Mayus()
+    {
+        Console.WriteLine("\nA continuación, escribe una oración para poder pasarla a mayúsculas");
+        string sentence = Console.ReadLine().ToUpper();
+
+        Console.WriteLine($"\nAquí tiene su oración: \"{sentence}\"");
+
+        
+    }
+
+}
+       
+                
+
+
+                        
+
+            
+
+            
+                
+
+
+
+    
+
+
